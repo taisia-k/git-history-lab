e231a52b60c5b5e98847ce71958d654ea2ea47bf
taisia-k <aataasaaq@gmail.com>
2025-12-27 01:55:36 +0300

    Добавлены функции умножения и вычитания

--- calculator.py
+++ calculator.py
@@ -1,2 +1,12 @@
 def add(a, b):
     return a + b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Деление на ноль невозможно")
+    return a / b
+
+def multiply(a, b):
+    return a * b
+
+
